0cfbcb3a92ed8ab793c9b37d5ca1808d76a4764b
pjmaia <pjmaia10@gmail.com>
2014-06-12 16:26:55 +0100

    Criada página Index de Carros; Feitas algumas ações ajax para a pesquisa;

--- src/java/dao/CityDAO.java
+++ src/java/dao/CityDAO.java
@@ -6,14 +6,15 @@ import java.util.TreeMap;
 import model.City;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
 
 public class CityDAO extends AbstractDAO<City> {
-
+    
     public Map<String,String> taxis(Integer idCountry) {
         Criteria crit = super.getSession().createCriteria(City.class);
         crit.add(Restrictions.conjunction()
-        .add(Restrictions.eq("country.id", idCountry))
-        .add(Restrictions.eq("isTaxi", true)));
+                .add(Restrictions.eq("country.id", idCountry))
+                .add(Restrictions.eq("isTaxi", true)));
         return createMapCities(crit.list());
     }
     
@@ -22,5 +23,12 @@ public class CityDAO extends AbstractDAO<City> {
         for(City c :list)  result.put(c.getId().toString(),c.getName());
         return result;
     }
-
+    
+    public Map<String,String> cities_with_subsidiaries (Integer country_id){
+        Criteria c = super.getSession().createCriteria(City.class,"city");        
+        c.createCriteria("subsidiaries", JoinType.INNER_JOIN);
+        c.add(Restrictions.eq("city.country.id", country_id));        
+        c.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        return createMapCities(c.list());
+    }
 }
--- src/java/dao/CountryDAO.java
+++ src/java/dao/CountryDAO.java
@@ -11,8 +11,15 @@ public class CountryDAO extends AbstractDAO<Country>{
     public List<Country> taxis(){
         Criteria c = super.getSession().createCriteria(Country.class,"country");
         c.createCriteria("cities", "city", JoinType.INNER_JOIN, Restrictions.eq("city.isTaxi", true));
-        c.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);        
-        return c.list();        
+        c.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        return c.list();
     }
     
+    public List<Country> countries_with_subs(){
+        Criteria c = super.getSession().createCriteria(Country.class,"country");
+        c.createCriteria("cities", "city", JoinType.INNER_JOIN);
+        c.createCriteria("city.subsidiaries", "sub", JoinType.INNER_JOIN);
+        c.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+        return c.list();
+    }
 }
